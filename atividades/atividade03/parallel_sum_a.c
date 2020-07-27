409b0438fa811b8430222b432ebb314dd966b256
flych3r <xavier_sampaio@alu.ufc.br>
2020-07-27 10:51:29 -0300

    atividade 3

--- atividades/atividade03/parallel_sum_a.c
+++ atividades/atividade03/parallel_sum_a.c
@@ -12,12 +12,14 @@ int main (int argc , char *argv[]) {
         #pragma omp parallel
         {
             int t = omp_get_thread_num ();
+
             int offset_lo = (remainder * (t + 0)) / ts;
             int offset_hi = (remainder * (t + 1)) / ts;
             int lo = (max / ts) * (t + 0) + 1 + offset_lo;
             int hi = (max / ts) * (t + 1) + 0 + offset_hi;
-            printf("thread: %d, lo: %d, hi: %d \n", t, lo, hi);
+
             sums[t] = 0;
+            printf("thread: %d, lo: %d, hi: %d \n", t, lo, hi);
             for (int i = lo; i <= hi; i++)
                 sums[t] = sums[t] + i;
         }
--- atividades/atividade03/parallel_sum_b.c
+++ atividades/atividade03/parallel_sum_b.c
@@ -5,17 +5,34 @@
 
 int main (int argc , char *argv[]) {
     int max;
-    int c = 4;
+    int chunk;
     sscanf (argv[1], "%d", &max);
+    sscanf (argv[2], "%d", &chunk);
     int ts = omp_get_max_threads ();
-    int sum = 0;
-    {
-        #pragma omp parallel for reduction(+:sum) schedule(static,c)
-        for (int i = 1; i <= max; i++){
-            printf ("thread: %d, iteration: %2d\n", omp_get_thread_num(), i);
-            sum = sum + i;
+    int sums[ts];
+    int remainder = max % ts;
+        #pragma omp parallel
+        {
+            int t = omp_get_thread_num ();
+
+            int lo = chunk * (t + 0) + 1;
+            int hi;
+            int step = chunk * ts;
+
+            sums[t] = 0;
+            for (int i = lo; i <= max; i+=step) {
+                int j = 0;
+                hi = j + i;
+                while (j < chunk && hi <= max) {
+                    printf("thread: %d, lo: %d, hi: %d \n", t, lo, hi);
+                    sums[t] = sums[t] + hi;
+                    j++;
+                    hi = j + i;
+                }
+            }
         }
-    }
-    printf ("%d\n", sum);
+    int sum = 0;
+    for (int t = 0; t < ts; t++) sum = sum + sums[t];
+    printf ("sum: %d\n", sum);
     return 0;
 }
