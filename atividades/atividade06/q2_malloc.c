56dc81bdaf08775cc86700eb9c280ffd96f56ef0
flych3r <xavier_sampaio@alu.ufc.br>
2020-08-31 16:22:22 -0300

    unable to do q2 with malloc

--- atividades/atividade06/q2_malloc.c
+++ atividades/atividade06/q2_malloc.c
@@ -4,14 +4,23 @@
 #include <mpi.h>
 
 int main(int argc, char *argv[]) {
-    int i, rank, size;
+    int i, j, rank, size;
+    int **input_array;
+    int **output_array;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    int input_array[size][2];
-    int output_array[size][2];
+
+    input_array = (int **)malloc(size * sizeof(int *));
+    output_array = (int **)malloc(size * sizeof(int *));
+
+    for (i = 0; i < size; i++)
+    {
+        input_array[i] = (int *) malloc(2 * sizeof(int));
+        output_array[i] = (int *) malloc(2 * sizeof(int));
+    }
 
     for (i = 0; i < size; i++)
     {
@@ -24,6 +33,8 @@ int main(int argc, char *argv[]) {
         printf("[%d %d]", input_array[i][0], input_array[i][1]);
     printf("\n");
 
+    MPI_Barrier(MPI_COMM_WORLD);
+
     MPI_Alltoall(input_array, 2, MPI_INT, output_array, 2, MPI_INT, MPI_COMM_WORLD);
 
     printf("Output for process %d\n", rank);
@@ -31,7 +42,11 @@ int main(int argc, char *argv[]) {
         printf("[%d %d]", output_array[i][0], output_array[i][1]);
     printf("\n");
 
-    MPI_Finalize();
+    // free(input_array[0]);
+    // free(input_array);
+    // free(output_array[0]);
+    // free(output_array);
 
+    MPI_Finalize();
     return 0;
 }
