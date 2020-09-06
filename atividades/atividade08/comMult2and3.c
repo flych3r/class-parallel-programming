8a1c4e3efcb915aa684e8163565c819051bf0cb4
flych3r <xavier_sampaio@alu.ufc.br>
2020-09-06 10:18:37 -0300

    changed split

--- atividades/atividade08/comMult2and3.c
+++ atividades/atividade08/comMult2and3.c
@@ -13,26 +13,30 @@ int main(int argc, char **argv) {
     int m3 = org_rank % 3 == 0;
     int m2_3 = m2 && m3;
 
-    MPI_Comm_split(MPI_COMM_WORLD, m2, 0, &mult2_comm);
-    MPI_Comm_split(MPI_COMM_WORLD, m3, 0, &mult3_comm);
-    MPI_Comm_split(MPI_COMM_WORLD, m2_3, 0, &mult2_3_comm);
+    MPI_Comm_split(MPI_COMM_WORLD, m2 ? 0 : MPI_UNDEFINED, 0, &mult2_comm);
+    MPI_Comm_split(MPI_COMM_WORLD, m3 ? 0 : MPI_UNDEFINED, 0, &mult3_comm);
+    MPI_Comm_split(MPI_COMM_WORLD, m2_3 ? 0 : MPI_UNDEFINED, 0, &mult2_3_comm);
 
-    MPI_Comm_size(mult2_comm, &new_size);
-    MPI_Comm_rank(mult2_comm, &new_rank);
-    printf("'MPI_COMM_WORLD' processo rank/quantidade %d/%d tem rank/quantidade %d/%d em 'mult2_comm '\n", org_rank, numprocs, new_rank, new_size);
+    if (mult2_comm != MPI_COMM_NULL) {
+        MPI_Comm_size(mult2_comm, &new_size);
+        MPI_Comm_rank(mult2_comm, &new_rank);
+        printf("'MPI_COMM_WORLD' processo rank/quantidade %d/%d tem rank/quantidade %d/%d em 'mult2_comm '\n", org_rank, numprocs, new_rank, new_size);
+        MPI_Comm_free(&mult2_comm);
+    }
 
-    MPI_Comm_size(mult3_comm, &new_size);
-    MPI_Comm_rank(mult3_comm, &new_rank);
-    printf("'MPI_COMM_WORLD' processo rank/quantidade %d/%d tem rank/quantidade %d/%d em 'mult3_comm '\n", org_rank, numprocs, new_rank, new_size);
+    if (mult3_comm != MPI_COMM_NULL) {
+        MPI_Comm_size(mult3_comm, &new_size);
+        MPI_Comm_rank(mult3_comm, &new_rank);
+        printf("'MPI_COMM_WORLD' processo rank/quantidade %d/%d tem rank/quantidade %d/%d em 'mult3_comm '\n", org_rank, numprocs, new_rank, new_size);
+        MPI_Comm_free(&mult3_comm);
+    }
 
-    MPI_Comm_size(mult2_3_comm, &new_size);
-    MPI_Comm_rank(mult2_3_comm, &new_rank);
-
-    printf("'MPI_COMM_WORLD' processo rank/quantidade %d/%d tem rank/quantidade %d/%d em 'mult2_3_comm '\n", org_rank, numprocs, new_rank, new_size);
-
-    MPI_Comm_free(&mult2_comm);
-    MPI_Comm_free(&mult3_comm);
-    MPI_Comm_free(&mult2_3_comm);
+    if (mult2_3_comm != MPI_COMM_NULL) {
+        MPI_Comm_size(mult2_3_comm, &new_size);
+        MPI_Comm_rank(mult2_3_comm, &new_rank);
+        printf("'MPI_COMM_WORLD' processo rank/quantidade %d/%d tem rank/quantidade %d/%d em 'mult2_3_comm '\n", org_rank, numprocs, new_rank, new_size);
+        MPI_Comm_free(&mult2_3_comm);
+    }
 
     MPI_Finalize();
     return 0;
